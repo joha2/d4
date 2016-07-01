855a938cbedf8cc262a962edc49f8dca90af5789
joha2 <joha2@gmx.net>
2016-07-01 15:01:52 +0200

    started tet class

--- d4.py
+++ d4.py
@@ -7,6 +7,12 @@ Created on Thu Nov 12 19:19:26 2015
 
 import numpy as np
 import math
+import pygame
+
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 # the n-d routines are implemented based on the stuff published at https://ef.gy/
 # and explained by Magnus Deininger in his ccc talk
@@ -68,8 +74,13 @@ def gen_translation_matrix(dim, trans):
     res[:dim,dim] = trans
     return res
 
-def gen_viewmatrix(dim, to, frm, eyeangle, *args):
-    res = np.dot(gen_translation_matrix(dim, -frm), np.dot(gen_homogen_matrix(3, calc_lookat(dim, to, frm, *args)),calc_perspective(dim, eyeangle)))
+def gen_viewmatrix_nd(dim, to, frm, eyeangle, *args):
+    res = np.dot(gen_translation_matrix(dim, -frm), np.dot(gen_homogen_matrix(dim, calc_lookat(dim, to, frm, *args)),calc_perspective(dim, eyeangle)))
+    return res
+
+
+def gen_viewmatrix_proj(dim, to, frm, *args):
+    res = np.dot(gen_translation_matrix(dim, -frm), gen_homogen_matrix(dim, calc_lookat(dim, to, frm, *args)))
     return res
 
 
@@ -81,7 +92,7 @@ def gen_rotation_matrix(dim, angle, u, v):
     return gen_homogen_matrix(dim, mat)
 
 
-class Camera4D(object):
+class Camera(object):
     """
     Camera class. Has one center of mass which can be translated.
     And has different rotation angles which are put into a matrix.
@@ -92,41 +103,192 @@ class Camera4D(object):
     """
     pass
 
-class PointCloud4D(object):
+class PointCloud(object):
     """
-    Class for managing 4D point transformations.
+    Class for managing nD point transformations.
     Geometric calculations are also part of this class.
     Projection into screen coordinates should not be missing.
-    Calculations are done via 5D homogenious coordinates.
+    Calculations are done via (n+1)D homogenious coordinates.
     All changes like translation and rotation are incremental.
     """
-    def __init__(self, points, com=np.array([0.,0.,0.,0.])):
-        self.points = np.vstack((points.T, np.ones(len(points)).T)).T
-        self.com = np.append(com, [1.])
+    def __init__(self):
         self.edges = []
-        self.faces = []
+
+    def setPoints(self, points):
+        self.points = points #np.vstack((points.T, np.ones(len(points)).T)).T
+        
 
     def addEdge(self, edges = (0,1)):
         if type(edges) == tuple and len(edges) == 2:
             self.edges.append(edges)
         elif type(edges) == list:
             self.edges.extend(edges)
+            
+    def getEdges(self):
+        return self.edges
 
-    def getEdgeProperties(self, edgenum=0):
+    def getEdgeProperties(self, edge):
         """
         Returns direction vector, startpoint
         and endpoint of an edge.
 
-        :param edgenum (int)
+        :param edge (tuple)
         :returns (direction, startpoint, endpoint)
         """
-
-        edge = self.edges[edgenum]
+        
         startpoint = self.points[edge[0]]
         endpoint = self.points[edge[1]]
 
         return (endpoint - startpoint, startpoint, endpoint)
+    
+    def getEdgeIntersectionAfterProj(self, dim, edge, proj):
 
+        eps = 1e-10
+        
+        projstart = np.dot(self.points[edge[0]], proj)
+        projend = np.dot(self.points[edge[1]], proj)
+        
+        print(projstart)
+        print(projend)
+
+        diffproj = projend[dim-1] - projstart[dim-1]
+        a = projstart[dim-1]
+
+        if abs(diffproj) < eps:
+            if abs(a) < eps:
+                t = 0.0 # t arbitrary and in particular t=0.0 (eigentlich muesste man a und b zurueckgeben)
+            else:
+                t = None # no solution
+        else:
+            t = - a/diffproj
+            if t < 0 or t > 1:
+                t = None # outside of edge
+
+        return t        
+        
+
+class Tet(PointCloud):
+    def __init__(self, dim, points):
+        super(Tet, self).__init__()
+        
+        shp = points.shape
+
+        print(shp)        
+
+
+        numpoints = shp[0]
+        dimpoints = shp[1]
+        
+        
+        if numpoints != dim+1 or dimpoints != dim:
+            pts = np.zeros((dim+1, dim+1))
+            for n in range(dim+1):
+                pts[n][dim] = 1.0
+                pts[n][n] = 1.0
+        else:
+            pts = points
+            print(points)
+            print(np.ones((numpoints,1)))
+            pts = np.concatenate((pts, np.ones((numpoints,1))), axis=1)
+
+        for n in range(dim+1):
+            for m in range(n):
+                self.addEdge((m, n))
+
+            
+        self.setPoints(pts)
+            
+        
+class PygameApp(object):
+    def __init__(self):
+        pygame.init()
+        self.display = (800,600)
+        self.screen = pygame.display.set_mode(self.display, DOUBLEBUF|OPENGL)
+
+        gluPerspective(90, (self.display[0]/self.display[1]), 0.1, 50.0)
+
+
+        #glTranslatef(0.0,0.0, -20.0) # -5
+
+
+        glEnable( GL_DEPTH_TEST )
+        #glEnable( GL_CULL_FACE )
+
+        #// enable color tracking
+        glEnable(GL_COLOR_MATERIAL)
+        #// set material properties which will be assigned by glColor
+    
+        glColorMaterial(GL_FRONT, GL_AMBIENT)
+        glColorMaterial(GL_FRONT, GL_DIFFUSE)
+        #glColorMaterial(GL_FRONT, GL_SPECULAR)
+
+
+        #// Create light components
+
+        ambientLight = ( 1.0, 1.0, 1.0, 1.0 )
+        diffuseLight = ( 0.8, 0.8, 0.8, 1.0 )
+        specularLight = ( 0.5, 0.5, 0.5, 1.0 )
+
+        #// Assign created components to GL_LIGHT0
+
+
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        
+    def drawText(self, position, fcolor, bcolor, textString):
+        font = pygame.font.Font (None, 64)
+        textSurface = font.render(textString, True, fcolor, bcolor)
+        textData = pygame.image.tostring(textSurface, "RGBA", True)
+
+        glWindowPos3f(*position)
+        glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
+        
+
+    def run(self):
+        
+        counter = 0        
+        
+        while True:
+
+            counter += 1
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.KEYDOWN: # one time key events
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                    if event.key == pygame.K_k:
+                        pass
+                    if event.key == pygame.K_l:
+                        pass
+                    if event.key == pygame.K_o:
+                        pass
+                    if event.key == pygame.K_p:
+                        pass
+
+            keys_pressed = pygame.key.get_pressed() 
+
+            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+
+            #glLightfv(GL_LIGHT0, GL_AMBIENT, self.ambientLight)
+            #glLightfv(GL_LIGHT0, GL_DIFFUSE, self.diffuseLight)
+            #glLightfv(GL_LIGHT0, GL_SPECULAR, self.specularLight)
+            #glLightfv(GL_LIGHT0, GL_POSITION, position)
+
+            self.drawText((400, 200, -100), (255, 0, 0, 255), (0, 0, 0, 0), str(counter))
+            self.drawText((350, 180, 0), (0, 255, 0, 255), (0, 0, 0, 0), str(counter))
+
+            pygame.display.flip()
+            pygame.time.wait(10)
+
+
+    def close(self):        
+        pass
+        
 
 
 def main():
@@ -149,7 +311,8 @@ def main():
     (-1, 1, 1, 1)
     ))
 
-    p = PointCloud4D(pts)
+    p = PointCloud()
+    p.setPoints(pts)
     p.addEdge(edges =
     [
     (0,1),
@@ -187,19 +350,21 @@ def main():
     ]
     )
 
-    #print(p.points)
-    #print(p.com)
-    #print(p.edges, len(p.edges))
-    print(calc_normals(4, np.array([1,0,0,0]), np.array([0,1,0,0]), np.array([0,0,1,0])))
 
-    bla = calc_lookat(3, np.array([1,1,1]), np.array([0,0,0]), np.array([0, -1, 1]))
-    print(bla)
-    print(gen_homogen_matrix(3, bla))
-    print(calc_perspective(4, 45*math.pi/180.0))
-    print(gen_viewmatrix(3, np.array([1,1,1]), np.array([0,0,0]), 45*math.pi/180.0, np.array([0, -1, 1])))
-    #print(gen_translation_matrix(4, np.array([1,2,3,4])))
-    print(gen_rotation_matrix(4, 0.1, np.array([1,0,0,0]),np.array([0,0,0,1])))
+    pm = gen_viewmatrix_proj(4, np.array([0,0,-10,0]), np.array([1/2,1/2,1/2,1/2]), np.array([1, 0, 0, 0]), np.array([0, 1, 0, 0]))
 
+    #t = Tet(4, np.array([[0,0,0,0], [1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]]))
+    #print(t.points)
+    #print(t.getEdges())
+    #print(t.getEdgeProperties(t.edges[9]))
+    #for a in t.edges:
+    #    print(t.getEdgeIntersectionAfterProj(4, a, pm))
+    #print("blub")
+
+    pg = PygameApp()
+    pg.run()
+    pg.close()
+    
 
 if __name__ == "__main__":
     main()
